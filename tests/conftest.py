81231b6c227dd1b17b883e5db4d3b78694ef9286
Bruno Rocha <rochacbruno@gmail.com>
2021-09-13 20:34:59 +0100

    More auth stuff

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
+import os
 import sys
 import pytest
 from fastapi.testclient import TestClient
 from typer.testing import CliRunner
-from project_name import app, cli
+
+# This next line ensures tests uses its own database and settings environment
+os.environ["FORCE_ENV_FOR_DYNACONF"] = "testing"  # noqa
+from project_name import app, cli, settings  # noqa
 
 
 # each test runs on cwd to its temp dir
@@ -17,14 +21,19 @@ def go_to_tmpdir(request):
         yield
 
 
-@pytest.fixture(scope="session", name="app")
+@pytest.fixture(scope="function", name="app")
 def _app():
     return app
 
 
-@pytest.fixture(scope="session", name="cli")
+@pytest.fixture(scope="function", name="cli")
 def _cli():
     return cli
+
+
+@pytest.fixture(scope="function", name="settings")
+def _settings():
+    return settings
 
 
 @pytest.fixture(scope="function")
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,7 @@
+def test_using_testing_db(settings):
+    assert settings.db.uri == "sqlite:///testing.db"
+
+
 def test_index(api_client):
     response = api_client.get("/")
     assert response.status_code == 200
