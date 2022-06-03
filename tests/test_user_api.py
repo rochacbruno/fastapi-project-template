49d984abb08a306133e0fd7a9d640ba12ada548a
Urjeet Patel <violentmagician.opensource@gmail.com>
2022-06-03 18:46:23 +0000

    test: :white_check_mark: Tests for querying users

--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -18,3 +18,27 @@ def test_user_create(api_client_authenticated):
     assert response.status_code == 200
     result = response.json()
     assert result["username"] == "foo"
+
+
+def test_get_my_profile(api_client_not_superuser):
+    response = api_client_not_superuser.get("/user/me")
+    assert response.status_code == 200
+    result = response.json()
+    assert result["username"] == "regular"
+    assert result["id"] == 3
+
+
+def test_get_other_profile_by_id(api_client_not_superuser):
+    response = api_client_not_superuser.get("/user/1")
+    assert response.status_code == 200
+    result = response.json()
+    assert result["username"] == "admin2"
+    assert result["id"] == 1
+
+
+def test_get_other_profile_by_name(api_client_not_superuser):
+    response = api_client_not_superuser.get("/user/admin")
+    assert response.status_code == 200
+    result = response.json()
+    assert result["username"] == "admin"
+    assert result["id"] == 2
